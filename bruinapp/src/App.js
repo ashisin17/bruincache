0a960d502072f1cceaf7bb73d8c74ad52b9d6657
Ashita Singh <ashisin17@gmail.com>
2023-11-11 16:28:15 -0800

    initialized routes + ex profile js and css set up

--- bruinapp/src/App.js
+++ bruinapp/src/App.js
@@ -1,7 +1,18 @@
-import  Map  from "./pages/Map"
+import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Profile from "./pages/profile.js"; 
+import Map from "./pages/Map"
+import "./App.css";
 
 export default function App () {
 	return (
-		<Map />
+		<Router>
+			{/* Set up your navigation or header component here if needed */}
+			<Routes>
+				<Route exact path="/" element={Map} />
+				<Route path="/profile" element={Profile} /> {/**Just add route to ur pgs */}
+			</Routes>
+		</Router>
 		)
 }
