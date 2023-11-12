c2fb86694ca43dc01bc5800ae3db00cf02eda678
audreyemu <mvp24601g@gmail.com>
2023-11-12 14:52:22 -0800

    Half of profile css

---
+++ bruinapp/src/pages/profile.js
@@ -0,0 +1,35 @@
+import './profile.css';
+
+export default function Profile() {
+    /* TODO: clean up profile.css to see what I can get rid of, figma is not super accurate...
+        Background
+        Task Bar
+
+        Profile Pic
+        Profile Text
+
+        Look at Caches Button
+    */
+    return(
+        <>
+            <div class="background-rectangle"></div>
+            <div class="task-bar"></div>
+
+            <div>
+                <div class="ellipse-3"></div>
+                <div class="ellipse-4"></div>
+                <div class="ellipse-5"></div>
+            </div>
+            <div class="profile-text">Insert text here</div>
+
+            <div>
+                <div class="submit-button-1"></div>
+                <div class="rectangle-21-1"></div>
+                <div class="submit-1">Look at Caches</div>
+                <div class="round-log-in-1"></div>
+                <div class="magnifying-glass"></div>
+                
+            </div>
+        </>
+    )
+}
