17546d0bab28bd8873e40ba200789f742af9a327
Ashita Singh <ashisin17@gmail.com>
2023-11-15 13:56:28 -0800

    Routes modified

--- bruinapp/src/App.js
+++ bruinapp/src/App.js
@@ -1,12 +1,18 @@
-import  Map  from "./pages/Map";
-import React from 'react';
-import Header from './pages/header';
-import Login from './pages/login';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Profile from "./pages/profile.js"; 
+import Map from "./pages/Map"
+import "./App.css";
 
 export default function App () {
 	return (
-		<Map />
+		<Router>
+			{/* Set up header component here if not header.tsx */}
+			<Routes>
+				<Route exact path="/" Component={Map} /> {/* replaced element with component for it to work :(( */}
+				<Route path="/profile" Component={Profile} /> {/**Just add route to ur pgs */}
+			</Routes>
+		</Router>
 		)
 }
