976094c5880a3fb4bde1936e38f67c58c4c10051
honss <aidanlevy03@gmail.com>
2023-11-15 14:19:03 -0800

    added page to create caches

--- bruinapp/src/pages/GMapComponent.jsx
+++ bruinapp/src/pages/GMapComponent.jsx
@@ -94,19 +94,7 @@ function GMapComponent({
 
 	return marker;
 	}
-   async function send () {
-	   //don't use get element and input forms like this, do it through react
-	   //I don't know how to do proper frontend, this is for backend testing only
-		await addDoc(collection(db, "caches"), {
-		  name: document.getElementById("name").value,
-		  location: {lat:document.getElementById("lat").value, lng:document.getElementById("lng").value},
-		  owner: document.getElementById("owner").value,
-		  desc: document.getElementById("desc").value,
-		  solves: 0,
-		  ratings: 0,
-		  rating: 0,
-		});
-  }
+   
   
   async function search_owner(name) {
 		const res = [];
@@ -145,17 +133,7 @@ function GMapComponent({
   	{
 		listPopups()
 	}
-	<p>PROOF of CONCEPT ONLY, so you can see how to do it. Make this secure and good and move it away <br/>
-	Just want you to see how the send function works </p><br></br>
-	<form>CREATE NEW CACHE
-	<p>Name:</p><input type="text" id="name"></input>
-	<p>Latitude:</p><input type="text" id="lat"></input>
-	<p>Longitude:</p><input type="text" id="lng"></input>
-	<p>Owner:</p><input type="text" id="owner"></input>
-	<p>Description:</p><input type="text" id="desc"></input>
 	
-	</form>
-	<button onClick={send}>SEND</button>
 	<p>SEARCH HERE </p>
 	<p>Owner:</p><form><input type="text" id="search_owner"></input></form>
 	<button onClick={() => search_owner(document.getElementById("search_owner").value)}>SEND</button>
