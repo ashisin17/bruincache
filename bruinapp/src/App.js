278405142675d5eeb393f7389fef7520ae02a6e8
ChanakyaG2004 <126143662+Kya2004@users.noreply.github.com>
2023-11-15 14:13:40 -0800

    Added taskbar again

--- bruinapp/src/App.js
+++ bruinapp/src/App.js
@@ -4,12 +4,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Profile from "./pages/profile.js"; 
 import Map from "./pages/Map"
 import Login from './pages/login';
+import Header from './pages/header';
 import "./App.css";
 
 export default function App () {
 	return (
 		<Router>
-			{/* Set up header component here if not header.tsx */}
+
+			<Header />
+
 			<Routes>
 				<Route exact path="/" Component={Map} /> {/* replaced element with component for it to work :(( */}
 				<Route path="/profile" Component={Profile} /> {/**Just add route to ur pgs */}
