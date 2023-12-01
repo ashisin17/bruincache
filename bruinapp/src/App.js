830ed70a4e00254013de3a5bf709123fb23779f7
Ashita Singh <ashisin17@gmail.com>
2023-12-01 12:57:05 -0800

    Firebase updates

--- bruinapp/src/App.js
+++ bruinapp/src/App.js
@@ -1,54 +1,25 @@
-import React from "react";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Profile from "./pages/profile.js"; 
-import Map from "./pages/Map"
-import app from "./firebase"
-import {signInWithGooglePopup} from './firebase';
-import {auth} from './firebase'
-import Login from './pages/login';
+import  Map  from "./pages/Map";
+import React from 'react';
 import Header from './pages/header';
-import Create from './pages/create';
-import Home from './pages/home';
-import { useState, useEffect } from 'react';
-import {signOut } from 'firebase/auth';
-import "./App.css";
+import Login from './pages/login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
-export default function App(props) {
-  const [user, setUser] = useState(null);
+export default function App () {
+	return (
+		<Router>
+		<div>
+			<div>
+			<Header />
 
-  const glogin = async () => {
-    const response = await signInWithGooglePopup();
-    console.log(user);
-  };
+				<Routes>
+					<Route path="/login" element={<Login />} />
+				</Routes>
+			
+			</div>
 
-  const gunlogin = async () => {
-    signOut(auth);
-  };
-
-  useEffect(() => {
-    auth.onAuthStateChanged((user) => {
-      setUser(user);
-    });
-  }, []);
-
-  return (
-    <div style={{ position: 'relative' }}>
-      <Router>
-        {user && <Header user={user} />}
-        {user ? (
-          <Routes>
-            <Route exact path="/" element={<Map user={user} />} />
-            <Route path="/profile" element={<Profile user = {user}/>} />
-            <Route path="/home" element={<Home />} />
-            <Route exact path="/create" element={<Create user={user} />} />
-          </Routes>
-        ) : (
-					<Login />
-        )}
-      </Router>
-    </div>
-  );
-
-}
+			<Map />
+		</div>
+		</Router>
+		);
+};
