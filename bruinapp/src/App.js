89e573ba4b0e6e605c80cf86942a08a604a0835d
honss <aidanlevy03@gmail.com>
2023-12-01 15:13:22 -0800

    fixed app routing

--- bruinapp/src/App.js
+++ bruinapp/src/App.js
@@ -1,25 +1,54 @@
-import  Map  from "./pages/Map";
-import React from 'react';
-import Header from './pages/header';
+import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Profile from "./pages/profile.js"; 
+import Map from "./pages/Map"
+import app from "./firebase"
+import {signInWithGooglePopup} from './firebase';
+import {auth} from './firebase'
 import Login from './pages/login';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './pages/header';
+import Create from './pages/create';
+import Home from './pages/home';
+import { useState, useEffect } from 'react';
+import {signOut } from 'firebase/auth';
+import "./App.css";
 
 
-export default function App () {
-	return (
-		<Router>
-		<div>
-			<div>
-			<Header />
+export default function App(props) {
+  const [user, setUser] = useState(null);
 
-				<Routes>
-					<Route path="/login" element={<Login />} />
-				</Routes>
-			
-			</div>
+  const glogin = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(user);
+  };
 
-			<Map />
-		</div>
-		</Router>
-		);
-};
+  const gunlogin = async () => {
+    signOut(auth);
+  };
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  }, []);
+
+  return (
+    <div style={{ position: 'relative' }}>
+      <Router>
+        {user && <Header user={user} />}
+        {user ? (
+          <Routes>
+            <Route exact path="/" element={<Map user={user} />} />
+            <Route path="/profile" element={<Profile user = {user}/>} />
+            <Route path="/home" element={<Home />} />
+            <Route exact path="/create" element={<Create user={user} />} />
+          </Routes>
+        ) : (
+					<Login />
+        )}
+      </Router>
+    </div>
+  );
+
+}
