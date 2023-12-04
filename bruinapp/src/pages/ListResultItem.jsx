303c802aec9bee11a365374954c3d16c9d475c2f
Ashita Singh <ashisin17@gmail.com>
2023-12-04 14:03:31 -0800

    Updated listing for caches

--- bruinapp/src/pages/ListResultItem.jsx
+++ bruinapp/src/pages/ListResultItem.jsx
@@ -3,7 +3,10 @@ function ListResultItem(props) {
 
     return (
         <li key={res.id}>
-            <h3>{res.id}</h3>
+            {/* <h3>{res.id}</h3> */}
+            <h3>Name: {res.name}</h3>
+            <p>Description: {res.description}</p>
+            <p>Location: {res.location}</p>
         </li>
     )
 } export default ListResultItem;
